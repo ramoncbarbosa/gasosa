6dbc4c4896cb9a524bf1c7f0ee0536bd09ece974
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2023-12-04 19:30:31 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,15 @@ export default function App() {
         <h1 className='text-white text-center'>Você sabe com qual combustível compensa mais abastecer seu carro?</h1>
         <h1 className='text-white text-center mb-4'>Utilize a calculadora abaixo:</h1>
         <div className='flex flex-col items-center'>
+  
           <label className='text-white'>Preço Litro Etanol R$</label>
-          <input className='w-96 rounded-lg mb-4' type="number" placeholder='0' onChange={(event) => setEtanol(event.target.value)}></input>
+          <input className='w-full max-w-md rounded-lg mb-4 px-4 py-2' type="number" placeholder='0' onChange={(event) => setEtanol(event.target.value)}></input>
+  
           <label className='text-white'>Preço Litro da Gasolina R$</label>
-          <input className='w-96 rounded-lg mb-4' type="number" placeholder='0' onChange={(event) => setGasolina(event.target.value)}></input>
+          <input className='w-full max-w-md rounded-lg mb-4 px-4 py-2' type="number" placeholder='0' onChange={(event) => setGasolina(event.target.value)}></input>
+  
           <Button undleClick={undleClick} />
-          
+  
           {modalButton && (
             <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
               <div className="bg-white p-8 rounded-lg text-center">
